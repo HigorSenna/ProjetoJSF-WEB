9522af2c6bd53f87568d20cea7e9cceb58b1e2ff
Higor Senna Chaves <hschaves@stefanini.com>
2016-04-27 10:35:59 -0300

    Lista Funcionando

--- src/main/java/br/com/stefanini/controller/FuncionarioController.java
+++ src/main/java/br/com/stefanini/controller/FuncionarioController.java
@@ -1,33 +1,43 @@
 package br.com.stefanini.controller;
 
-
+import java.util.ArrayList;
 import java.util.List;
+
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+
 import br.com.stefanini.model.Funcionario;
 
-@ManagedBean
+@ManagedBean(name = "funcionarioController")
 @ViewScoped
-public class FuncionarioController{	
+public class FuncionarioController {
 	private Funcionario funcionario = new Funcionario();
-	private List<Funcionario> listaFuncionarios;	
+	private List<Funcionario> funcionarios = new ArrayList<>();
 
 	public Funcionario getFuncionario() {
 		return funcionario;
-	}	
+	}
 
 	public void setFuncionario(Funcionario funcionario) {
 		this.funcionario = funcionario;
-	}	
-	
-	public void addF(){
-		listaFuncionarios.add(funcionario);		
 	}
-	
-	public List<Funcionario> pegarLista(){
-		return listaFuncionarios;
+
+	public void addFuncionario() {
+		funcionarios.add(funcionario);
+		funcionario = new Funcionario();
 	}
-	
+
+	public List<Funcionario> pegarLista() {
+		return funcionarios;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
 }
